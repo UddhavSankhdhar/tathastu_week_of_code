5164a2b88d6ec2e478e16338a8cbbfa90e1c406c
UddhavSankhdhar <65585006+UddhavSankhdhar@users.noreply.github.com>
2020-05-24 11:59:35 +0530

    Update Program3.py

--- Day2/Program3.py
+++ Day2/Program3.py
@@ -1,5 +1,5 @@
 n = int(input("Enter the length of one arm of pattern X- "))
 for i in range(n):
-    print(" "*i + "*" + " "*(n - 1 - i) + "*")
+    print(" "*i + "*" + "  "*(n - 1 - i) + "*")
 for i in range(n):
-    print(" "*(n - 1 - i) + "*" + " "*i + "*")
+    print(" "*(n - 1 - i) + "*" + "  "*i + "*")
