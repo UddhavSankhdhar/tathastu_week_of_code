3d244484bb54336abbfabe66ea71263733fdd637
UddhavSankhdhar <65585006+UddhavSankhdhar@users.noreply.github.com>
2020-05-24 07:02:26 +0530

    Update Program4.py

--- Day1/Program4.py
+++ Day1/Program4.py
@@ -3,4 +3,4 @@ SP=float(input("Enter Selling price-"))
 Profit=SP-CP
 NewSP=1.05*Profit + CP
 print("Profit=",Profit)
-print("The Cost price for 5% increase in profit should be=", NewSP)
+print("The Selling price for 5% increase in profit should be=", NewSP)
