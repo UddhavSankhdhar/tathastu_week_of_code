a3dfae8dc7ebd1f7d5d8c678b1708ab0275d299f
UddhavSankhdhar <65585006+UddhavSankhdhar@users.noreply.github.com>
2020-05-24 08:29:50 +0530

    Create Program5.py

---
+++ Day1/Program5.py
@@ -0,0 +1,15 @@
+P1=int(input("Enter runs scored by batsman 1- "))
+P2=int(input("Enter runs scored by batsman 2- "))
+P3=int(input("Enter rUns scored by batsman 3- "))
+StrikeRateP1=P1*1.66
+StrikeRateP2=P2*1.66
+StrikeRateP3=P3*1.66
+print("Strike rate of batsman 1-",StrikeRateP1)
+print("Strike rate of batsman 2-",StrikeRateP2)
+print("Strike rate of batsman 3-",StrikeRateP3)
+print("Runs scored in 120 balls by batsman 1-",P1 * 2)
+print("Runs scored in 120 balls by batsman 2-",P2 * 2)
+print("Runs scored in 120 balls by batsman 3-",P3 * 2)
+print("Maximum number of sixes hit by batsman 1-",P1 // 6)
+print("Maximum number of sixes hit by batsman 2-",P2 // 6)
+print("Maximum number of sixes hit by batsman 3-",P3 // 6)
