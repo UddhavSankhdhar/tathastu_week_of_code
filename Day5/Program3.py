11952be306f6d342b35fc4b98ec3eb7806a3dc67
UddhavSankhdhar <65585006+UddhavSankhdhar@users.noreply.github.com>
2020-05-24 18:31:54 +0530

    Create Program3.py

---
+++ Day5/Program3.py
@@ -0,0 +1,13 @@
+def H(List):
+    if len(List) == 2:
+        return max(List)
+    if len(List) == 1:
+        return List[0]
+    if len(List) == 3:
+        return max(List[1], List[0] + H(List[2:]))
+    return max(List[1] + H(List[3:]), List[0] + H(List[2:]))
+size = int(input("Enter the number of houses in a row- "))
+List = []
+for i in range(size):
+    List.append(int(input("Enter the house number value- " + str(i + 1) + ": ")))
+print("The maximum stolen value from houses-", H(List))
